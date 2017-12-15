6e76d06353482c5b22be9ca6787c4b8a4f757319
Gabriel Couto <gabrielcoutos@yahoo.com.br>
2017-12-15 02:21:08 -0200

    Modals prontos

--- js/app.js
+++ js/app.js
@@ -18,15 +18,35 @@ $(document).ready(function () {
         var nome = $(this).text() + "/";
         $.get("https://pokeapi.co/api/v2/pokemon/" + nome, function (pokemon) {
             $("#modal-nome").text(pokemon.name);
-
             $("#modal-imagem").append("<img src='" + pokemon.sprites.front_default + "' class='img-rounded'>");
             console.log(pokemon);
             console.log(pokemon.name);
+            $.each(pokemon.types, function (index, value) {
+                $("#modal-tipo").append(value.type.name + "/");
+            });
+            $("#modal-habilidades").append("<h6>Abilities: </h6>");
             $.each(pokemon.abilities, function (index, value) {
-                console.log(value.ability.name);
+                $("#modal-habilidades").append(value.ability.name + " / ");
+            });
+            $("#modal-estatistica").append("<h6>Stats: </h6>");
+            $("#modal-estatistica").append("<div class='col-md-3'>" +
+                "<p >weight:" + pokemon.weight + "</p>" +
+                "</div>");
+            $.each(pokemon.stats, function (index, value) {
+                $("#modal-estatistica").append("<div class='col-md-3'>" +
+                    "<p>" + value.stat.name + ": " + value.base_stat + "</p>" +
+                    "</div>");
+            });
+            $("#moves").append("<h6>Moves: </h6>");
+            $.each(pokemon.moves, function (index, value) {
+                $("#moves").append(value.move.name+" / ");
             });
 
         });
+        $("#moves").empty();
+        $("#modal-habilidades").empty();
+        $("#modal-estatistica").empty();
+        $("#modal-tipo").text("Tipo:");
         $("#modal-imagem").empty();
     });
 
