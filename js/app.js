44037594ce52cc10fd4baa4a8c2200cbe757957f
Gabriel Couto <gabrielcoutos@yahoo.com.br>
2017-12-15 00:48:12 -0200

    imagens

--- js/app.js
+++ js/app.js
@@ -1,30 +1,33 @@
-$(document).ready(function () {    
-    $.get("https://pokeapi.co/api/v2/pokemon/", function (resposta) {       
+$(document).ready(function () {
+    $.get("https://pokeapi.co/api/v2/pokemon/", function (resposta) {
         console.log(resposta.results);
         $.each(resposta.results, function (index, value) {
-            $("#pokemons").append("<div class='panel-group' id='accordion'>"+
-            "<div class='panel panel-default'>"+
-               " <div class='panel-heading'>"+
-                    "<h4 class='panel-title'>"+
-                        "<a data-toggle='collapse' data-parent='#accordion' href='#"+value.name+"' class='poke'>"+value.name+
-                            "</a>"+
-                    "</h4>"+
-                "</div>"+
-                "<div id='"+value.name+"' class='panel-collapse collapse '>"+
-                    "<div class='panel-body'>"+"ronaldo"+
-                    "</div>"+
-                "</div>"+
-            "</div>"+
-        "</div>");
+            $(".panel-group").append(
+                "<div class='panel panel-default'>" +
+                " <div class='panel-heading'>" +
+                "<h4 class='panel-title'>" +
+                "<a data-toggle='modal' data-parent='#accordion' data-target='#myModal' class='poke'>" + value.name +
+                "</a>" +
+                "</h4>" +
+                "</div>"
+            );
         });
-    });    
-
-    $(document).on('click',".poke",function(){
-        var nome=$(this).text()+"/";
-        $.get("https://pokeapi.co/api/v2/pokemon/"+nome,function(pokemon){
-        $("#"+nome).children[0].text("ola");
-        });
-     
     });
-    
+
+    $(document).on('click', ".poke", function () {
+        var nome = $(this).text() + "/";
+        $.get("https://pokeapi.co/api/v2/pokemon/" + nome, function (pokemon) {
+            $("#modal-nome").text(pokemon.name);
+
+            $("#modal-imagem").append("<img src='" + pokemon.sprites.front_default + "' class='img-rounded'>");
+            console.log(pokemon);
+            console.log(pokemon.name);
+            $.each(pokemon.abilities, function (index, value) {
+                console.log(value.ability.name);
+            });
+
+        });
+        $("#modal-imagem").empty();
+    });
+
 });
