5ed6380810ed80495e8be0dfdf35b662814219e1
Gabriel Couto <gabrielcoutos@yahoo.com.br>
2017-12-15 14:30:39 -0200

    paginacao

--- js/app.js
+++ js/app.js
@@ -1,29 +1,10 @@
 $(document).ready(function () {
     $("#loader-starter").hide();
     $("#loader-modal").hide();
-    $.ajax({
-        url: "https://pokeapi.co/api/v2/pokemon/",
-        method: 'GET',
-        dataType: 'JSON',
-        beforeSend: function () {
-            $("#loader-starter").show();
-        },
-        success: function (resposta, status) {
-            $("#loader-starter").hide();
-            console.log(resposta.results);
-            $.each(resposta.results, function (index, value) {
-                $(".panel-group").append(
-                    "<div class='panel panel-default'>" +
-                    " <div class='panel-heading'>" +
-                    "<h4 class='panel-title'>" +
-                    "<a data-toggle='modal' data-parent='#accordion' data-target='#myModal' class='poke'>" + value.name +
-                    "</a>" +
-                    "</h4>" +
-                    "</div>"
-                );
-            });
-        },       
-    });
+    carregarElementos(null);
+
+
+
     $(document).on('click', ".poke", function () {
         var nome = $(this).text() + "/";
         $.ajax({
@@ -70,6 +51,67 @@ $(document).ready(function () {
         $("#modal-estatistica").empty();
         $("#modal-tipo").text("Tipo:");
         $("#modal-imagem").empty();
+    });
+
+    function carregarElementos(elemento) {
+        var urlAPI = "https://pokeapi.co/api/v2/pokemon/";
+        var limit = 20;
+        if (elemento !== null) {
+            var offset = (elemento - 1) * limit;
+            urlAPI += "?limit=" + limit + "&offset=" + offset;           
+        }
+        $(".panel-group").empty();
+        $.ajax({
+            url: urlAPI,
+            method: 'GET',
+            dataType: 'JSON',
+            beforeSend: function () {
+                $("#loader-starter").show();
+            },
+            success: function (resposta, status) {
+                $("#loader-starter").hide();
+                console.log(resposta);
+                var maximoPagina = Math.ceil(resposta.count / limit);
+                $("#ultimo").text(maximoPagina);
+                $.each(resposta.results, function (index, value) {
+                    $(".panel-group").append(
+                        "<div class=col-md-6>" +
+                        "<div class='panel panel-default'>" +
+                        " <div class='panel-heading'>" +
+                        "<h4 class='text-center panel-title'>" +
+                        "<a data-toggle='modal' data-parent='#accordion' data-target='#myModal' class='poke'>" + value.name +
+                        "</a>" +
+                        "</h4>" +
+                        "</div>" +
+                        "</div>"
+                    );
+                });
+            },
+        });
+    }
+
+    $(document).on('click', '.page-link', function () {
+        var pagina = $(this).text();
+        carregarElementos(pagina);
+        if(pagina===1 || pagina===3 || pagina===4){
+            $(".dot").addClass("page-link");
+            $(".dot").text("2");
+            $(".before").text("3");
+            $(".midle").text("4");               
+            $(".after").text("5");
+        }
+
+        if (pagina >= 5 && pagina<= 47) {
+            $(".dot").removeClass("page-link");
+            $(".dot").text("...");
+            var resultado = Number(pagina)+1;
+            $(".before").text(pagina-1);
+            $(".midle").text(pagina);  
+            $(".midle").parent().addClass("active");     
+            $(".after").text(resultado);
+            
+        }
+
     });
 
 });
